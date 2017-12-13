7e48ead559c3e0a1e2f2554b369e5ef0ef71c3e6
Josh Grauer <josh.grauer@ctca-hope.com>
2017-12-13 16:20:49 -0700

    Started adding the portfolio section.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,6 +8,31 @@ function init() {
             document.getElementById('name').textContent = config.name;
             document.getElementById('about-photo').innerHTML = `<img src="../images/${ config.about.photo }" />`;
             document.getElementById('about-description').innerHTML = config.about.description;
+            config.portfolio.projects.forEach(project => {
+                const projectContainer = document.createElement('div');
+
+                const projectName = document.createElement('div');
+                projectName.className = 'project-name';
+                projectName.textContent = project.name;
+                projectContainer.appendChild(projectName);
+
+                const projectDescription = document.createElement('div');
+                projectDescription.className = 'project-description';
+                projectDescription.innerHTML = project.description;
+                projectContainer.appendChild(projectDescription);
+
+                const projectScreenshots = document.createElement('div');
+                projectScreenshots.className = 'project-screenshots';
+                project.screenshots.forEach(screenshot => {
+                    const projectScreenshot = document.createElement('img');
+                    projectScreenshot.className = 'project-screenshot';
+                    projectScreenshot.setAttribute('src', screenshot);
+                    projectScreenshots.appendChild(projectScreenshot);
+                });
+                projectContainer.appendChild(projectScreenshots);
+
+                document.getElementById('portfolio-section').appendChild(projectContainer);
+            });
             
             ScrollSpy.init();
         });
